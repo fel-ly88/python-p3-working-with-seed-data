78e9affd68a273c0874a44bc90844d72ab6d94c8
Your Name <your-email@example.com>
2025-08-24 22:23:08 +0300

    add seed.py

--- lib/seed.py
+++ lib/seed.py
@@ -8,9 +8,29 @@ from sqlalchemy.orm import sessionmaker
 from models import Game
 
 fake = Faker()
+games = [
+    Game(
+        title=fake.name(),
+        genre=fake.word(),
+        platform=fake.word(),
+        price=random.randint(0, 60)
+    )
+    for _ in range(50)
+]
 
-if __name__ == '__main__':
-    
-    engine = create_engine('sqlite:///seed_db.db')
-    Session = sessionmaker(bind=engine)
-    session = Session()
+
+
+session.query(Game).delete()
+session.commit()
+
+
+# Single object
+botw = Game(title="Breath of the Wild", platform="Switch", genre="Adventure", price=60)
+session.add(botw)
+session.commit()
+
+# Multiple objects
+ffvii = Game(title="Final Fantasy VII", platform="Playstation", genre="RPG", price=30)
+mk8 = Game(title="Mario Kart 8", platform="Switch", genre="Racing", price=50)
+session.bulk_save_objects([ffvii, mk8])
+session.commit()
